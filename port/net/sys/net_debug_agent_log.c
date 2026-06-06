0c2db02add5399ad914ab216a8c71b700b17fa92
Alex Vanderveen <alex@technicallycomputers.ca>
2026-06-06 03:11:04 -0400

    Update net_debug_agent_log.c

--- port/net/sys/net_debug_agent_log.c
+++ port/net/sys/net_debug_agent_log.c
@@ -1,14 +1,40 @@
 #include <sys/net_debug_agent_log.h>
 
 #include <stdio.h>
+
+#ifdef _WIN32
+#ifndef WIN32_LEAN_AND_MEAN
+#define WIN32_LEAN_AND_MEAN
+#endif
+#include <windows.h>
+#else
 #include <sys/time.h>
+#endif
 
 #define NET_DEBUG_AGENT_LOG_PATH "/home/alex/Documents/GitHub/BattleShip/.cursor/debug-3b7f79.log"
+
+static long long net_debug_agent_log_timestamp_ms(void)
+{
+#ifdef _WIN32
+	FILETIME ft;
+	ULARGE_INTEGER uli;
+
+	GetSystemTimeAsFileTime(&ft);
+	uli.LowPart = ft.dwLowDateTime;
+	uli.HighPart = ft.dwHighDateTime;
+	uli.QuadPart -= 116444736000000000ULL;
+	return (long long)(uli.QuadPart / 10000ULL);
+#else
+	struct timeval tv;
+
+	gettimeofday(&tv, NULL);
+	return (long long)tv.tv_sec * 1000LL + (long long)(tv.tv_usec / 1000);
+#endif
+}
 
 void net_debug_agent_log_line(const char *hypothesis_id, const char *location, const char *message,
                               const char *data_json)
 {
-	struct timeval tv;
 	FILE *f;
 	long long ts;
 
@@ -16,8 +42,7 @@ void net_debug_agent_log_line(const char *hypothesis_id, const char *location, c
 	{
 		return;
 	}
-	gettimeofday(&tv, NULL);
-	ts = (long long)tv.tv_sec * 1000LL + (long long)(tv.tv_usec / 1000);
+	ts = net_debug_agent_log_timestamp_ms();
 	f = fopen(NET_DEBUG_AGENT_LOG_PATH, "a");
 	if (f == NULL)
 	{
